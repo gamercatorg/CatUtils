cad53c3c0f6b0c1c39b1e0d13a1739aaa78c6266
Gamer4life Puvilland <uyenwontanswer@gmail.com>
2020-02-08 17:34:01 -0800

    Added base code

--- src/main/java/io/github/gamer4life1/CatUtils/Main.java
+++ src/main/java/io/github/gamer4life1/CatUtils/Main.java
@@ -1,4 +1,14 @@
 package io.github.gamer4life1.CatUtils;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("CatUtils has been enabled!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("CatUtils has been disabled!");
+    }
 }

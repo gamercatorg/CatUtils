d4e0e51f1b011bc45f24653ccddf01e0b84ad8e3
Gamer4life Puvilland <uyenwontanswer@gmail.com>
2020-02-09 19:59:43 -0800

    Added fly command

--- src/main/java/io/github/gamer4life1/CatUtils/FlyCommand.java
+++ src/main/java/io/github/gamer4life1/CatUtils/FlyCommand.java
@@ -1,20 +1,22 @@
 package io.github.gamer4life1.CatUtils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class FeedCommand implements CommandExecutor {
+public class FlyCommand implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (command.getName().equalsIgnoreCase("feed")) {
+        if (command.getName().equalsIgnoreCase("fly")) {
             if (commandSender instanceof Player) {
                 Player p = (Player) commandSender;
-                p.setSaturation(20);
-            } else {
-                commandSender.sendMessage("You need to be a player to do that!");
+                if (!p.getAllowFlight()) {
+                    p.setAllowFlight(true);
+                } else {
+                    p.setAllowFlight(false);
+                }
             }
-            return true;
         }
         return false;
     }

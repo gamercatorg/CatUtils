0b5d34bf1c6b1851d2f8570e12653d934b13df58
Gamer4life Puvilland <uyenwontanswer@gmail.com>
2020-02-10 15:43:02 -0800

    Fixed saturatiom

--- src/main/java/io/github/gamer4life1/CatUtils/FeedCommand.java
+++ src/main/java/io/github/gamer4life1/CatUtils/FeedCommand.java
@@ -4,13 +4,15 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class FeedCommand implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (command.getName().equalsIgnoreCase("feed")) {
             if (commandSender instanceof Player) {
                 Player p = (Player) commandSender;
-                p.setSaturation(20);
+                p.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 255, 255));
             } else {
                 commandSender.sendMessage("You need to be a player to do that!");
             }

17226842231da9804250778e2e0382d25bb4c09c
Gamer4life <lpuvilla0001@mymail.lausd.net>
2020-02-11 18:19:55 +0000

    Fixed unused imports

--- src/main/java/io/github/gamer4life1/CatUtils/FeedCommand.java
+++ src/main/java/io/github/gamer4life1/CatUtils/FeedCommand.java
@@ -4,8 +4,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 public class FeedCommand implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {

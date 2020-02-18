ebc71d879ceca50a3bbccbef2be68517177454f0
Gamer4life <lpuvilla0001@mymail.lausd.net>
2020-02-18 21:17:13 +0000

    Never mind

--- src/main/java/io/github/gamer4life1/CatUtils/FlyCommand.java
+++ src/main/java/io/github/gamer4life1/CatUtils/FlyCommand.java
@@ -5,17 +5,31 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class FlyCommand implements CommandExecutor {
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("fly")) {
-            if (commandSender instanceof Player) {
+            if (commandSender instanceof Player && args.length == 0) {
                 Player p = (Player) commandSender;
                 if (!p.getAllowFlight()) {
                     p.setAllowFlight(true);
+                    p.sendMessage("Fly mode enabled!");
+                    return true;
                 } else {
                     p.setAllowFlight(false);
+                    p.sendMessage("Fly mode disabled!");
+                    return true;
                 }
-            } else commandSender.sendMessage("You need to be a player to do that!");
+            } else {
+                if (args.length == 0) {
+                    commandSender.sendMessage(ChatColor.RED + "Correct usage: /fly <player>");
+                    return false;
+                }
+
+                
+
+            }
         }
         return false;
     }

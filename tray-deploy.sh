d3e484145c974baf7bd697a7b4851bf314f76ea3
Gamer4life <uyenwontanswer@gmail.com>
2020-02-19 18:47:20 +0000

    Fixed slashes

--- tray-deploy.sh
+++ tray-deploy.sh
@@ -1,5 +1,5 @@
 JAR_VERSION=$1
 mvn $MAVEN_CLI_OPTS clean package
-curl -X POST -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/packages/gamer4life/CatUtils/CatUtils/versions --data {\"name\" : \"${JAR_VERSION}\", \"desc\": \"Next version.\"}
+curl -X POST -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/packages/gamer4life/CatUtils/CatUtils/versions --data "{\"name\" : \"${JAR_VERSION}\", \"desc\": \"Next version.\"}"
 curl -T target/*.jar -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/content/gamer4life/CatUtils/CatUtils/$JAR_VERSION/CatUtils-$JAR_VERSION.jar
-curl -X POST -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/content/gamer4life/CatUtils/CatUtils/$JAR_VERSION/publish -d { \"discard\": \"false\" }
+curl -X POST -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/content/gamer4life/CatUtils/CatUtils/$JAR_VERSION/publish -d "{ \"discard\": \"false\" }"

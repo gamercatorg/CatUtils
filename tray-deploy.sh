a358f2841be7fc0ed90e26d2b0407482e0ec7f55
Gamer4life <uyenwontanswer@gmail.com>
2020-02-20 05:15:21 +0000

    Changed deploy user

--- tray-deploy.sh
+++ tray-deploy.sh
@@ -1,5 +1,5 @@
 JAR_VERSION=$1
-curl -H "Content-Type: application/json" -X POST -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/packages/gamer4life/CatUtils/CatUtils/versions --data "{\"name\" : \"${JAR_VERSION}\", \"desc\": \"Next version.\"}"
-curl -X DELETE -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/content/gamer4life/CatUtils/CatUtils-$JAR_VERSION.jar
-curl -T target/*.jar -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/content/gamer4life/CatUtils/CatUtils/$JAR_VERSION/CatUtils-$JAR_VERSION.jar
-curl -H "Content-Type: application/json" -X POST -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/content/gamer4life/CatUtils/CatUtils/$JAR_VERSION/publish -d "{ \"discard\": \"false\" }"
+curl -H "Content-Type: application/json" -X POST -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/packages/gamercatorg/CatUtils/CatUtils/versions --data "{\"name\" : \"${JAR_VERSION}\", \"desc\": \"A new version.\"}"
+curl -X DELETE -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/content/gamercatorg/CatUtils/CatUtils-$JAR_VERSION.jar
+curl -T target/*.jar -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/content/gamercatorg/CatUtils/CatUtils/$JAR_VERSION/CatUtils-$JAR_VERSION.jar
+curl -H "Content-Type: application/json" -X POST -u gamer4life:29ce028e88c860e37602a1e9deab1bffe4ea91a0 https://api.bintray.com/content/gamercatorg/CatUtils/CatUtils/$JAR_VERSION/publish -d "{ \"discard\": \"false\" }"

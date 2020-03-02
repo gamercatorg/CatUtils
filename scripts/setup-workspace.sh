242c9c8bdab666adc1565acccb95220c97d049c8
Gamer4life <lpuvilla0001@mymail.lausd.net>
2020-03-02 19:06:32 +0000

    Signed commits

--- scripts/setup-workspace.sh
+++ scripts/setup-workspace.sh
@@ -2,3 +2,6 @@ git remote add lab https://l-bot:1yfM4rwxUz4kx5szA6yV@gitlab.com/gamer4life1/cat
 git pull
 git pull lab master
 npm install -g jfrog-cli-go
+gpg --import private.asc
+git config --config user.signingkey 76C07A52ECF60538
+git config --global commit.gpgsign true
